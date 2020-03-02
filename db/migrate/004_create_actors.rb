733d61243df07cf6c52dea2c7915597aa8be7bec
Gary Wheeler <gkwheeler40@gmail.com>
2020-03-02 10:47:09 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -2,9 +2,8 @@ class CreateActors < ActiveRecord::Migration[5.1]
   
   def change
     create_column :actors do |t|
-      t.string :name 
-      t.integer :actor_id 
-      t.integer :show_id
+      t.string :first_name
+      t.string :last_name
     end
   end
 end 
