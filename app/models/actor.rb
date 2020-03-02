437ba6b1101454478c9576ea2f6263376872583e
Gary Wheeler <gkwheeler40@gmail.com>
2020-03-02 11:21:10 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,12 @@
 class Actor < ActiveRecord::Base
   has_many :characters
   has_many :shows, through: :characters
+  
+  def self.full_name
+    "#{first_name} #{last_name}"
+  end
+  
+  def self.list_roles
+    
+  end 
 end
