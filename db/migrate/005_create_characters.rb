d0e1c49acac81de870f353ae6d3337dbb2f76f42
Gary Wheeler <gkwheeler40@gmail.com>
2020-03-02 11:11:25 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,7 +1,7 @@
 class CreateCharacters < ActiveRecord::Migration[5.1]
   
   def change
-    create_column :characters do |t|
+    create_table :characters do |t|
       t.string :name 
       t.integer :actor_id 
       t.integer :show_id
