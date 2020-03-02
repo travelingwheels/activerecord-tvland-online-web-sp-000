879f3d5ea8f9bcb6d6fc54689dd596286916e8ec
Gary Wheeler <gkwheeler40@gmail.com>
2020-03-02 10:45:22 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,1 +1,10 @@
-class CreateActors 
+class CreateActors < ActiveRecord::Migration[5.1]
+  
+  def change
+    create_column :actors do |t|
+      t.string :name 
+      t.integer :actor_id 
+      t.integer :show_id
+    end
+  end
+end 
