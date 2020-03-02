4e77a91a3970d10096b6f6b853c6ec398cb6be0c
Gary Wheeler <gkwheeler40@gmail.com>
2020-03-02 11:23:28 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,6 +7,8 @@ class Actor < ActiveRecord::Base
   end
   
   def list_roles
-    characters.collect do |
+    characters.collect do |character|
+      "#{character.name} - #{character.show.name}"
+    end
   end 
 end
